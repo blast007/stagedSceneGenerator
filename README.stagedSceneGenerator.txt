0b62cd7358ea5777e9d08bc93312afaa7da9dc10
Scott Wichser <blast007@users.sourceforge.net>
2018-12-15 09:53:24 -0600

    Initial release

---
+++ README.stagedSceneGenerator.txt
@@ -0,0 +1,90 @@
+BZFlag Server Plugin: stagedSceneGenerator
+================================================================================
+
+This BZFlag plugin allows setting up staged scenes with fixed tanks and shots
+for generating screenshots.
+
+LIMITATIONS:
+ - Super bullet in experimental mode will not look correct as the geoshot's
+   length is based on shot speed.
+ - Guided missiles may also have visual differences as the trails may be based
+   on the shot speed.
+ - Since shots are generated on the server the shot limit is 255.
+ - Presently, flags are not attached to a tank. So even though you can make a
+   tank appear to shoot another shot type (laser, SW, SB), the staged tank will
+   not be holding a flag.
+ - Can not simulate a zoned tank as the API does not allow toggling this.
+ - Simulating a shot that has ricocheted will require manual pre-computing and
+   will not show the rico effect on the wall. For laser this isn't a problem as
+   you can fire a laser from the normal muzzle position and it will ricochet as
+   normal.
+ - Because of the suggestion to use -noTeamKills, it is possible that otherwise
+   lethal laser shots (like a ricocheting laser or a shockwave) will not kill.
+
+Loading the plugin
+--------------------------------------------------------------------------------
+
+This plugin requires a configuration file (see stagedSceneGenerator.cfg for an
+example of the format) as well as a couple other bzfs options. Since we may be
+generating server-side shots near other tanks (such as to simulate a tank
+shooting a laser) we want to disallow teamkilling. To prevent AI tanks from
+shooting on their own, we also want to disallow shooting.
+
+So pass these options to bzfs, either via the command line or the bzfs config:
+  -noTeamKills -ms 0
+
+Then to load the plugin, specify a configuration file using this format:
+  -loadplugin stagedSceneGenerator,<configfilename>
+For example:
+  -loadplugin stagedSceneGenerator,/path/to/my/stagedSceneGenerator.cfg
+
+
+Running the game client
+--------------------------------------------------------------------------------
+To populate the staged tanks, you may use solo bots from the game client. Start
+the game client from the command line, adjusting the 5 to reflect how many
+staged tanks you need to create:
+  bzflag -solo 5
+
+Then join your server. To turn off most of the user interface, bring up the
+chat prompt in the game with the 'n' key and type:
+  /localset noGUI 1
+You can then also turn off the radar and chat console with F1 and F2.
+
+
+Configuration File
+--------------------------------------------------------------------------------
+
+The configuration file uses an INI-like format. Most options are optional,
+though you'll generally want at least a team, position, and rotation. Note the
+language below with MUST and MAY, with MUST meaning required and MAY being
+optional.
+
+Each staged tank or shot MUST start with a unique section name contained in
+square brackets:
+    [SomeName]
+
+Then a type MUST be specified, either tank or shot.
+    type = tank
+  or
+    type = shot
+
+For both types, a team, position (pos) or rotation (rot) MAY be provided. The
+team takes a lowercase string of either red, green, blue, purple, hunter,
+rabbit, or rogue. The position takes three floating point numbers,
+corresponding to the x, y, and z coordinates in the game, with z being height.
+The rotation is represented in degree.
+    team = purple
+    pos = 10 20 30
+    rot = 180
+
+For tanks, there is the option of using random spawns instead of specifying
+the position and rotation. By default this is turned off, but to enable it:
+    random = true
+
+For shots, there are two additional options. You MAY specify the flag type of
+the shot, so it is possible to use other shot types. Review the limitations
+section at the top of the file for some caveats. You MAY also set the elevation
+angle of a shot, with 0 being level with the ground and 90 being straight up.
+    flag = L
+    elev = 45
---
+++ stagedSceneGenerator.cpp
@@ -0,0 +1,367 @@
+// stagedSceneGenerator 0.5 (2018-12-15)
+// This BZFlag plugin allows setting up staged scenes with fixed tanks and
+// shots for generating screenshots. See README.stagedSceneGenerator.txt
+
+/*
+Copyright (c) 2018 Scott Wichser
+All rights reserved.
+
+Redistribution and use in source and binary forms, with or without
+modification, are permitted provided that the following conditions are met:
+
+1. Redistributions of source code must retain the above copyright notice, this
+   list of conditions and the following disclaimer.
+2. Redistributions in binary form must reproduce the above copyright notice,
+   this list of conditions and the following disclaimer in the documentation
+   and/or other materials provided with the distribution.
+
+THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" AND
+ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED
+WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
+DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT OWNER OR CONTRIBUTORS BE LIABLE FOR
+ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES
+(INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES;
+LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND
+ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
+(INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS
+SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+
+The views and conclusions contained in the software and documentation are those
+of the authors and should not be interpreted as representing official policies,
+either expressed or implied, of the FreeBSD Project.
+
+*/
+
+#include "bzfsAPI.h"
+#include "plugin_utils.h"
+
+#include <math.h>
+#include <vector>
+
+class stagedSceneGenerator : public bz_Plugin
+{
+public:
+    virtual const char* Name ()
+    {
+        return "Staged Scene Generator";
+    }
+    virtual void Init ( const char* config );
+
+    virtual void Event ( bz_EventData * eventData );
+
+private:
+    bool readConfig(const char* configFile);
+    bz_eTeamType teamFromString(std::string team);
+
+    PluginConfig config;
+
+    struct StagedPlayer
+    {
+        bz_eTeamType team{eNoTeam};
+        bool random{false};
+        float pos[3] {0.0f, 0.0f, 0.0f};
+        float rot{0.0f};
+
+        int playerID{-1};
+    };
+
+    std::vector<StagedPlayer> stagedPlayers;
+
+    struct StagedShot
+    {
+        bz_eTeamType team{eNoTeam};
+        float pos[3] {0.0f, 0.0f, 0.0f};
+        float dir[3] {1.0f, 0.0f, 0.0f};
+        std::string flag{""};
+    };
+
+    std::vector<StagedShot> stagedShots;
+
+    double lastShotsFired = -9999.0f;
+};
+
+BZ_PLUGIN(stagedSceneGenerator)
+
+void stagedSceneGenerator::Init ( const char* commandLine )
+{
+    bz_debugMessage(4,"stagedSceneGenerator plugin loaded");
+
+    // A configuration file is required
+    if (commandLine == NULL || strlen(commandLine) == 0)
+    {
+        bz_debugMessage(0, "ERROR: stagedSceneGenerator plugin needs a config file");
+        bz_shutdown();
+        return;
+    }
+    else
+    {
+        // Try to read the configuration file
+        if (!readConfig(commandLine))
+        {
+            bz_debugMessage(0, "ERROR: There was an error reading the provided stagedSceneGenerator config file");
+            bz_shutdown();
+            return;
+        }
+
+        // init events here with Register();
+        Register(bz_eGetAutoTeamEvent);
+        Register(bz_eGetPlayerSpawnPosEvent);
+        Register(bz_ePlayerUpdateEvent);
+        Register(bz_ePlayerPartEvent);
+        Register(bz_eTickEvent);
+
+        // Allow bots
+        bz_updateBZDBBool("_disableBots", false);
+
+        // Disable those pesky height checks so we can go wild
+        bz_updateBZDBBool("_disableHeightChecks", true);
+
+        // Set some values that affect tanks and shots
+        bz_updateBZDBDouble("_gravity", -0.000001);
+        bz_updateBZDBDouble("_shotSpeed", 0.01);
+        bz_updateBZDBDouble("_shotRange", 0.05);
+        bz_updateBZDBDouble("_laserAdLife", 1.0);
+    }
+}
+
+bool stagedSceneGenerator::readConfig(const char* configFile)
+{
+    // Parse the configuration
+    config.read(configFile);
+
+    // If we had any errors, bail out
+    if (config.errors > 0)
+        return false;
+
+    // Loop through each section of the configuration. There will be one section per tank or shot.
+    for (auto section : config.getSections())
+    {
+        // Store a lowercase copy of the type for comparisons
+        std::string type = makelower(config.item(section, "type").c_str());
+
+        // Congratulations Mr. and Mrs. Abrams, it's a tank
+        if (type == "tank")
+        {
+            StagedPlayer p;
+
+            p.team = teamFromString(config.item(section, "team"));
+
+            // A tank can be randomly spawned or set to spawn at a fixed location
+            p.random = (makelower(config.item(section, "random").c_str()) == "true");
+
+            // If it's not random, read the position and rotation, if set
+            if (!p.random)
+            {
+                std::string pos = config.item(section, "pos");
+                if (pos.size() > 0)
+                {
+                    std::vector<std::string> pos2 = tokenize(pos, std::string(" "), 3, false);
+                    if (pos2.size() == 3)
+                    {
+                        p.pos[0] = atof(pos2.at(0).c_str());
+                        p.pos[1] = atof(pos2.at(1).c_str());
+                        p.pos[2] = atof(pos2.at(2).c_str());
+                    }
+                }
+
+                std::string rot = config.item(section, "rot");
+                if (rot.size() > 0)
+                    p.rot = atof(rot.c_str());
+            }
+
+            // Add this staged player to our list
+            stagedPlayers.push_back(p);
+        }
+        // Guess I'll take a shot at this
+        else if (type == "shot")
+        {
+            StagedShot s;
+
+            s.team = teamFromString(config.item(section, "team"));
+
+            // A flag abbreviation can be provided to change the shot type
+            s.flag = config.item(section, "flag");
+
+            std::string pos = config.item(section, "pos");
+            if (pos.size() > 0)
+            {
+                std::vector<std::string> pos2 = tokenize(pos, std::string(" "), 3, false);
+                if (pos2.size() == 3)
+                {
+                    s.pos[0] = atof(pos2.at(0).c_str());
+                    s.pos[1] = atof(pos2.at(1).c_str());
+                    s.pos[2] = atof(pos2.at(2).c_str());
+                }
+            }
+
+            std::string strRot = config.item(section, "rot");
+            std::string strElev = config.item(section, "elev");
+            double rot = 0, elev = 0;
+            if (strRot.size() > 0)
+                rot = atof(strRot.c_str()) * M_PI / 180.0;
+
+            if (strElev.size() > 0)
+                elev = (atof(strElev.c_str()) * M_PI / 180.0);
+            elev += M_PI / 2;
+
+            s.dir[0] = sin(elev) * cos(rot);
+            s.dir[1] = sin(elev) * sin(rot);
+            s.dir[2] = -cos(elev);
+
+            // Add this staged shot to our list
+            stagedShots.push_back(s);
+        }
+
+    }
+
+    return true;
+}
+
+bz_eTeamType stagedSceneGenerator::teamFromString(std::string team)
+{
+    team = makelower(team.c_str());
+    if (team == "red") return eRedTeam;
+    else if (team == "green") return eGreenTeam;
+    else if (team == "blue") return eBlueTeam;
+    else if (team == "purple") return ePurpleTeam;
+    else if (team == "hunter") return eHunterTeam;
+    else if (team == "rabbit") return eRabbitTeam;
+
+    return eRogueTeam;
+}
+
+void stagedSceneGenerator::Event(bz_EventData *eventData)
+{
+    switch(eventData->eventType)
+    {
+    case bz_eGetAutoTeamEvent:
+    {
+        bz_GetAutoTeamEventData_V1* data = (bz_GetAutoTeamEventData_V1*)eventData;
+        // Ignore clients joining as observer
+        if (data->team != eObservers)
+        {
+            bz_debugMessagef(0, "INFO: Requested team for %d is %d", data->playerID, data->team);
+            // Find the first staged player that does not have an associated player
+            for (auto &stagedPlayer : stagedPlayers)
+            {
+                // We found an available staged player
+                if (stagedPlayer.playerID == -1)
+                {
+                    bz_debugMessagef(0, "INFO: Found available staged player for %d", data->playerID);
+                    stagedPlayer.playerID = data->playerID;
+                    data->team = stagedPlayer.team;
+                    data->handled = true;
+                    break;
+                }
+            }
+
+            // If all the staged players are ... staged ... then just make this player an observer.
+            // This has the positive side effect of also making extra -solo bots go away.
+            if (!data->handled)
+            {
+                bz_debugMessagef(0, "WARNING: No available staged player for %d, so assigning observer", data->playerID);
+                data->team = eObservers;
+                data->handled = true;
+            }
+        }
+        break;
+    }
+
+    case bz_eGetPlayerSpawnPosEvent:
+    {
+        bz_GetPlayerSpawnPosEventData_V1* data = (bz_GetPlayerSpawnPosEventData_V1*)eventData;
+
+        // See if this is a staged player
+        for (auto stagedPlayer : stagedPlayers)
+        {
+            // We found a match!
+            if (stagedPlayer.playerID == data->playerID)
+            {
+                bz_debugMessagef(0, "INFO: Spawning staged player %d", data->playerID);
+
+                // If this isn't a random spawn, set the position and rotation
+                if (!stagedPlayer.random)
+                {
+                    data->pos[0] = stagedPlayer.pos[0];
+                    data->pos[1] = stagedPlayer.pos[1];
+                    data->pos[2] = stagedPlayer.pos[2];
+                    data->rot = stagedPlayer.rot * M_PI / 180.0f;
+                }
+
+                // Spawn the tank slightly in the air so that it won't be moving around
+                data->pos[2] += 0.01;
+
+                data->handled = true;
+
+                // Break out of the 'for' loop
+                break;
+            }
+        }
+        break;
+    }
+
+    case bz_ePlayerUpdateEvent:
+    {
+        bz_PlayerUpdateEventData_V1* data = (bz_PlayerUpdateEventData_V1*)eventData;
+
+        // We spawn tanks in the air, and have gravity set real low. Eventually a tank might land and start
+        // moving, so kill 'em if they do.
+        if (data->state.status == eAlive && (data->state.velocity[0] != 0.0f || data->state.velocity[1] != 0.0f))
+        {
+            bz_debugMessagef(0, "NOTE: Killing player '%s' because they moved", bz_getPlayerCallsign(data->playerID));
+            bz_killPlayer(data->playerID, false);
+        }
+        break;
+    }
+
+    case bz_ePlayerPartEvent:
+    {
+        bz_PlayerJoinPartEventData_V1* data = (bz_PlayerJoinPartEventData_V1*)eventData;
+
+        // When a player leaves, see if they were assigned to a staged player and release them
+        for (auto &stagedPlayer : stagedPlayers)
+        {
+            if (stagedPlayer.playerID == data->playerID)
+                stagedPlayer.playerID = -1;
+        }
+
+        break;
+    }
+
+    case bz_eTickEvent:
+    {
+        bz_TickEventData_V1* data = (bz_TickEventData_V1*)eventData;
+
+        // I'ma firing my BLAAAAARRRR
+        if (data->eventTime > lastShotsFired + bz_getBZDBDouble("_reloadTime"))
+        {
+            lastShotsFired = data->eventTime;
+            for (auto stagedShot : stagedShots)
+            {
+                // The laser's length is based on shot speed, so reset the shot speed for this shot
+                if (stagedShot.flag == "L")
+                    bz_resetBZDBVar("_shotSpeed");
+
+                // FIRE!!!
+                bz_fireServerShot(stagedShot.flag.c_str(), stagedShot.pos, stagedShot.dir, stagedShot.team);
+                bz_debugMessagef(1, "Firing shot at %f %f %f", stagedShot.pos[0], stagedShot.pos[1], stagedShot.pos[2]);
+
+                // If we just shot a laser, remember to set the shot speed again
+                if (stagedShot.flag == "L")
+                    bz_updateBZDBDouble("_shotSpeed", 0.01);
+            }
+        }
+    }
+
+    default:
+        break;
+    }
+}
+
+// Local Variables: ***
+// mode: C++ ***
+// tab-width: 4 ***
+// c-basic-offset: 4 ***
+// indent-tabs-mode: nil ***
+// End: ***
+// ex: shiftwidth=4 tabstop=4
